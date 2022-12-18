49ed611e7a079c4739c56791e898b7a218f66030
f-acepalm <f.acepalm@mail.ru>
2022-12-18 14:08:30 +0400

    1 services (#1)
    * Add a Coupon service initial code
    
    * Add coupon support to api services
    
    * Add data migration
    
    * Add coupon to web app
    
    * Fix auth
    
    * Fix errors

---
+++ src/Services/Coupon/Coupon.API/IntegrationEvents/Events/OrderStatusChangedToAwaitingCouponValidationIntegrationEvent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.eShopOnContainers.BuildingBlocks.EventBus.Events;
+using Newtonsoft.Json;
+
+namespace Coupon.API.IntegrationEvents.Events
+{
+    public record OrderStatusChangedToAwaitingCouponValidationIntegrationEvent : IntegrationEvent
+    {
+        public int OrderId { get; private set; }
+
+        public string OrderStatus { get; private set; }
+
+        public string BuyerName { get; private set; }
+
+        public string Code { get; private set; }
+
+        public OrderStatusChangedToAwaitingCouponValidationIntegrationEvent(int orderId, string orderStatus, string buyerName, string code)
+        {
+            OrderId = orderId;
+            OrderStatus = orderStatus;
+            BuyerName = buyerName;
+            Code = code;
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.SignalrHub/IntegrationEvents/Events/OrderStatusChangedToAwaitingCouponValidationIntegrationEvent.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+
+namespace Ordering.SignalrHub.IntegrationEvents.Events;
+public record OrderStatusChangedToAwaitingCouponValidationIntegrationEvent : IntegrationEvent
+{
+    public int OrderId { get; private set; }
+
+    public string OrderStatus { get; private set; }
+
+    public string BuyerName { get; private set; }
+
+    public string Code { get; private set; }
+
+    public OrderStatusChangedToAwaitingCouponValidationIntegrationEvent(int orderId, string orderStatus, string buyerName, string code)
+    {
+        OrderId = orderId;
+        OrderStatus = orderStatus;
+        BuyerName = buyerName;
+        Code = code;
+    }
+}
